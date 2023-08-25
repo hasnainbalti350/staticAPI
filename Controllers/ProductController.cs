44cb1ab9e300f8d43a7ca18b4083177fbc733dac
hasnainbalti350 <hasnainbalti350@gmail.com>
2023-08-25 12:27:21 +0500

    StaticApi

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using staticWebApi.Models;
+
+namespace staticWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> products = new List<Product>();
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault(p => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        [HttpPost]
+        public IActionResult CreateProduct(Product product)
+        {
+            product.Id = products.Count()+1;
+            products.Add(product);
+            return Ok(products);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateProduct(int id, Product product)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            existingProduct.Name = product.Name;
+            existingProduct.Price = product.Price;
+            existingProduct.Description = product.Description;
+            return Ok(existingProduct);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteProduct(int id)
+        {
+            var existingProduct = products.FirstOrDefault(p => p.Id == id);
+            if (existingProduct == null)
+            {
+                return NotFound();
+            }
+
+            products.Remove(existingProduct);
+            if (existingProduct != null) 
+            {
+                return Ok(products);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+    }
+}
